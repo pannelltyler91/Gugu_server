edbc6b53d1102bcddbfa223e12a88ff16df8fc53
unknown <you@example.com>
2022-09-08 00:26:15 -0400

    completed movies route create,read,delete

--- routes/movies.js
+++ routes/movies.js
@@ -16,14 +16,39 @@ router.post('/add', (req,res) => {
     }).then((movie) =>{
         if(movie.length === 0){
             //add movie to watchlist
+            db.movies.create({
+                title:req.body.title,
+                imdbID:req.body.id,
+                poster_url:req.body.poster
+            })
+            res.status(201).json({message:'added to list'})
         }else{
             //return message that movie already exists
+            res.status(401).json({message:'movie already added to list'})
         }
     })
-    res.json({message:'it works'})
+})
+
+router.get('/', (req,res) =>{
+    console.log(req.body)
+    db.movies.findAll({}).then((movies) => {
+        res.json({message:'got movies', watchList:movies})
+    })
+    res.json({message:'it worked'})
 })
 
 
+router.delete('/:id', (req,res) =>{
+    console.log(req.params.id)
+    db.movies.delete({
+        where:{
+            imdbID:req.params.id
+        }
+    }).then((movie) =>{
+        res.status(201).json({message:'removed from watchList succesfully'})
+    })
+    
+})
 
 
 
