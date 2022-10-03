6297c0188cc14665041c8e47a6e007fb101aa70b
unknown <you@example.com>
2022-10-03 17:04:45 -0400

    working on calendar post route, but running into error

--- routes/calendar.js
+++ routes/calendar.js
@@ -6,6 +6,30 @@ const db = require("../models");
 router.use(cors());
 router.use(express.json());
 
+router.get('/:date', (res,req) =>{
+    db.calendar.findAll({
+        where:{
+            date:req.params.date
+        }
+    }).then((events) => {
+        if(events.length > 0){
+            res.status(201).json({events:events, message:"got events"})
+        }else{
+            res.status(404).json({message:'no events exist for this date'})
+        }
+    })
+})
+
+router.post('/add', (req,res) =>{
+    console.log(req.body, 'this is working')
+     res.json({message:'its working'})
+  db.calendar.findAll({
+
+     }).then((events) =>{
+        console.log(events)
+     })
+})
+
 
 
 
